ee9d358e86ccde7b1e224fbe14a0f491f634fc96
Nicolas Lima <nichollaslimma734@gmail.com>
2022-12-06 16:38:42 -0300

    implementado sql alchemy a aplicacao, juntamente com sua conexao e representacao das tabelas do db.

--- jogoteca.py
+++ jogoteca.py
@@ -1,43 +1,47 @@
 from flask import Flask, render_template, request, redirect, session, flash, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__) 
 app.secret_key = 'lira'
 #esse name faz uma referência ao próprio arquivo, garantindo que isso vai fazer rodar a aplicação.
 
-class Jogo:
-    def __init__(self,nome, categoria, console):
-        self.nome = nome
-        self.categoria = categoria
-        self.console = console
 
-jogo1 = Jogo("Skyrim", "RPG", "PC")
-jogo2 = Jogo("Fallout new vegas", "RPG", "PC")
-jogo3 =Jogo("The Witcher 3", "RPG", "PC")
-lista = [jogo1, jogo2, jogo3]
+# configurações de conexão do banco
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    '{SGDB}://{usuario}:{senha}@{servidor}/{database}'.format(
+        SGDB = 'mysql+mysqlconnector',
+        usuario = 'root',
+        senha = '123456',
+        servidor = 'localhost',
+        database = 'jogoteca'
+    )
 
-class Usuario:
-    def __init__(self, nome, username, senha):
-        self.nome = nome
-        self.username = username
-        self.senha = senha
+# cria a conexão entre a aplicação e o mysql
+db = SQLAlchemy(app)
 
-usuario1 = Usuario("Nicolas", "Nicolas734", "12345")
-usuario2 = Usuario("Natalia", "A LIRA", "lira")
-usuario3 = Usuario("Rodrigo", "Digao_calcada", "calcada")
-usuario4 = Usuario("Raniel", "Agro_boy", "agro_tec")
-usuario5 = Usuario("Rafael", "veio", "rafel")
 
-usuarios = {
-    usuario1.username:usuario1,
-    usuario2.username:usuario2,
-    usuario3.username:usuario3,
-    usuario4.username:usuario4,
-    usuario5.username:usuario5
-    }
+# representação das tabelas do banco de dados
+class Jogos(db.Model):
+    id = db.Column(db.Integer, primary_key = True, autoincrement=True)
+    nome = db.Column(db.String(50), nullable = False)
+    categoria = db.Column(db.String(40), nullable = False)
+    console = db.Column(db.String(20), nullable = False)
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+class Usuarios(db.Model):
+    username = db.Column(db.String(50), primary_key = True)
+    nome = db.Column(db.String(50), nullable = False)
+    senha = db.Column(db.String(100), nullable = False)
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
 
 @app.route('/')
 def index():
-    return render_template('lista.html', titulo='Jogos', jogos=lista, titulo_pagina="Jogoteca")
+    return render_template('lista.html', titulo='Jogos', titulo_pagina="Jogoteca")
 
 
 @app.route('/novo_jogo')
